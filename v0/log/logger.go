872fd8cae6f73cc4df6d25d538b546236207880e
Sean Kell <sean.michael.kell@gmail.com>
2013-01-05 10:01:24 -0500

    Add log level utility Log methods to Logger.
    Add disable level method and checks for log level being enabled.
    
    Return an error if the log level does not exist on any Log(x) calls.

--- v0/log/logger.go
+++ v0/log/logger.go
@@ -3,6 +3,7 @@
 package log
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"os"
@@ -34,41 +35,103 @@ type Logger struct {
 	levels map[int]*logLevel
 }
 
-// Log writes to the log level given by the first argument; or to the standard logger if the given log level does not 
-// exist. Other arguments are handled in the manner of fmt.Print.
-func (logger *Logger) Log(logLevel int, v ...interface{}) {
-	level := logger.levels[logLevel]
-	if level != nil {
-		level.logger.Print(v...)
-	} else {
-		log.Print(v...)
+func (logger *Logger) DisableLevel(l int) {
+	if level, ok := logger.levels[l]; ok {
+		level.enabled = false
 	}
 }
 
-// Logf writes to the log level given by the first argument; or to the standard logger if the given log level does not
-// exist. Other arguments are handled in the manner of fmt.Printf.
-func (logger *Logger) Logf(logLevel int, format string, v ...interface{}) {
+// Log writes to the log level given by the first argument. Other arguments are handled in the manner of fmt.Print. 
+// It does nothing if the log level is disabled; and returns an error if the log level does not exist.
+func (logger *Logger) Log(logLevel int, v ...interface{}) error {
 	level := logger.levels[logLevel]
 	if level != nil {
-		level.logger.Printf(format, v...)
-	} else {
-		log.Printf(format, v...)
+		if level.enabled {
+			level.logger.Print(v...)
+		}
+		return nil
 	}
+
+	return fmt.Errorf("No log level '%d' exists in this logger.", logLevel)
+}
+
+// Debug writes to the debug log level. It handles arguments in the manner of fmt.Print.
+// It does nothing if debug is disabled. It panics if the debug log level does not exist.
+func (logger *Logger) Debug(v ...interface{}) {
+	if logger.Log(LogLevelDebug, v...) != nil {
+		panic("Logger must have Debug log level.")
+	}
+}
+
+// Info writes to the info log level. It handles arguments in the manner of fmt.Print.
+// It does nothing if info is disabled. It panics if the info log level does not exist.
+func (logger *Logger) Info(v ...interface{}) {
+	if logger.Log(LogLevelInfo, v...) != nil {
+		panic("Logger must have Info log level.")
+	}
+}
+
+// Warn writes to the warn log level. It handles arguments in the manner of fmt.Print.
+// It does nothing if warn is disabled. It panics if the warn log level does not exist.
+func (logger *Logger) Warn(v ...interface{}) {
+	if logger.Log(LogLevelWarn, v...) != nil {
+		panic("Logger must have Warn log level.")
+	}
+}
+
+// Error writes to the error log level. It handles arguments in the manner of fmt.Print.
+// It does nothing if error is disabled. It panics if the error log level does not exist.
+func (logger *Logger) Error(v ...interface{}) {
+	if logger.Log(LogLevelError, v...) != nil {
+		panic("Logger must have Error log level.")
+	}
+}
+
+// Panics writes to the error log level, then panics with the formatted string. It handles arguments in the manner of 
+// fmt.Print.
+func (logger *Logger) Panic(v ...interface{}) {
+	s := fmt.Sprint(v...)
+	logger.Log(LogLevelError, s)
+	panic(s)
+}
+
+// Fatal writes to the error log level; then exits with os.Exit(1). It handles arguments in the manner of fmt.Print.
+func (logger *Logger) Fatal(v ...interface{}) {
+	logger.Log(LogLevelError, v...)
+	os.Exit(1)
+}
+
+// Logf writes to the log level given by the first argument. Other arguments are handled in the manner of fmt.Printf.
+// Logf does nothing if the log level specified is disabled; it returns an error if the log level does not exist.
+func (logger *Logger) Logf(logLevel int, format string, v ...interface{}) error {
+	level := logger.levels[logLevel]
+	if level != nil {
+		if level.enabled {
+			level.logger.Printf(format, v...)
+		}
+		return nil
+	}
+
+	return fmt.Errorf("No log level '%d' exists in this logger.", logLevel)
 }
 
 // Logln writes to the log level given by the first argument. Other arguments are handled in the manner of fmt.Println.
-func (logger *Logger) Logln(logLevel int, v ...interface{}) {
+func (logger *Logger) Logln(logLevel int, v ...interface{}) error {
 	level := logger.levels[logLevel]
 	if level != nil {
-		level.logger.Println(v...)
-	} else {
-		log.Println(v...)
+		if level.enabled {
+			level.logger.Println(v...)
+		}
+		return nil
 	}
+
+	return fmt.Errorf("No log level '%d' exists in this logger.", logLevel)
 }
 
 type logLevel struct {
-	level  int
-	logger *log.Logger
+	level   int
+	logger  *log.Logger
+	enabled bool
 }
 
 // CreateLogger allocates a new logger object and adds it to the cache. 
@@ -76,10 +139,10 @@ func CreateLogger(name string, out io.Writer) *Logger {
 	l := &Logger{
 		Name: name,
 		levels: map[int]*logLevel{
-			LogLevelDebug: &logLevel{LogLevelDebug, log.New(out, PrefixLogLevelDebug, log.LstdFlags)},
-			LogLevelInfo:  &logLevel{LogLevelInfo, log.New(out, PrefixLogLevelInfo, log.LstdFlags)},
-			LogLevelWarn:  &logLevel{LogLevelWarn, log.New(out, PrefixLogLevelWarn, log.LstdFlags)},
-			LogLevelError: &logLevel{LogLevelError, log.New(out, PrefixLogLevelError, log.LstdFlags)},
+			LogLevelDebug: &logLevel{LogLevelDebug, log.New(out, PrefixLogLevelDebug, log.LstdFlags), true},
+			LogLevelInfo:  &logLevel{LogLevelInfo, log.New(out, PrefixLogLevelInfo, log.LstdFlags), true},
+			LogLevelWarn:  &logLevel{LogLevelWarn, log.New(out, PrefixLogLevelWarn, log.LstdFlags), true},
+			LogLevelError: &logLevel{LogLevelError, log.New(out, PrefixLogLevelError, log.LstdFlags), true},
 		},
 	}
 
